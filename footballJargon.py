c32c9b0a6bde03fe508cc359d98223d3e813cb8c
mxch18 <maxens.achiepi@gmx.com>
2022-11-04 22:31:03 +0100

    [UTIL] - Add dict for position acronyms

---
+++ footballJargon.py
@@ -0,0 +1,5 @@
+positions = {"QB": "Quarterback", "RB": "Running Back", "WR": "Wide Receiver",
+             "TE": "Tight End", "DB": "Defensive Back", "K": "Kicker",
+             "LB": "Linebacker", "DL": "Defensive Line", "OL": "Offensive Line",
+             "LS": "Long Snapper", "P": "Punter"
+             }
