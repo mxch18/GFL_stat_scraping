3da48f6ccdd4527ce6176e1928768b51d760f67e
mxch18 <maxens.achiepi@gmx.com>
2023-02-20 20:35:14 +0100

    [CHORE] - Clean up my act

--- footballJargon.py
+++
@@ -1,5 +0,0 @@
-positions = {"QB": "Quarterback", "RB": "Running Back", "WR": "Wide Receiver",
-             "TE": "Tight End", "DB": "Defensive Back", "K": "Kicker",
-             "LB": "Linebacker", "DL": "Defensive Line", "OL": "Offensive Line",
-             "LS": "Long Snapper", "P": "Punter"
-             }
