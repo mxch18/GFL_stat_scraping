0cf88bb9fb2afcc09322d67e8e4037d70cebbf89
mxch18 <maxens.achiepi@gmx.com>
2023-03-01 20:31:23 +0100

    [REFACTOR] - Rename patterns and split some of them

--- tests/text_processor.py
+++ tests/text_processor.py
@@ -7,17 +7,43 @@ if __name__ == '__main__':
                 'touchback')
 
     # extract all patterns from the sentence
-    kickoff_pattern = re.compile(
-     r"[A-Z]\.[A-Za-z\-]+\skickoff\s\d+\syard(s)*\sto\sthe\s[A-Z]+\d+")
-    return_pattern = re.compile(
-     r"[A-Z]\.[A-Za-z\-]+\sreturn\s\d+\syard(s)*\sto\sthe\s[A-Z]+\d+")
-    tackle_pattern = re.compile(
+    pattern_kickoff = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ kickoff \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_return = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ return \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_tackle = re.compile(
      r"(?<=(\(|\;))[A-Z]\.[A-Za-z\-]+(?=(\)|\;))")
     # player pattern preceded by ( or ; and followed by ) or ;
-    extraPoint_pattern = re.compile(
-     r"[A-Z]\.[A-Za-z\-]+\s(kick|rush)\sattempt\s(good|failed)")
-    rush_pattern = re.compile(
-     r"[A-Z]\.[A-Za-z\-]+\srush\s.*for\s(((loss\sof\s)*\d+\syard(s)*)|(no\sgain))\sto\sthe\s[A-Z]+\d+")
+    pattern_extraPoint = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ (kick|rush) attempt (good|failed)")
+    # rush
+    # r"[A-Z]\.[A-Za-z\-]+ rush .*for (((loss of )*\d+ yard(s)*)|(no gain)) to the [A-Z]+\d+"
+    pattern_rush_gain = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ rush.*for \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_rush_noGain = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ rush.*for no gain to the [A-Z]+\d+")
+    pattern_rush_loss = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ rush.*for loss of \d+ yard(s)* to the [A-Z]+\d+")
+
+    pattern_firstDown = re.compile(
+     r"1ST DOWN")
+    pattern_pass_complete = re.compile(
+     r"")
+    pattern_pass_incomplete = re.compile(
+     r"")
+
+    pattern_sack = re.compile(
+     r"[A-Z]\.[A-Za-z\-]+ sacked for loss of \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_punt = re.compile(
+     r"")
+    pattern_fg = re.compile(
+     r"")
+    pattern_recovered = re.compile(
+     r"")
+    pattern_fairCatch = re.compile(
+     r"")
+    pattern_safety = re.compile(
+     r"")
 
     with open('./test_sentences.txt', 'r') as sentences_file:
         sentences = sentences_file.readlines()
