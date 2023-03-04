9bbe5ecccd5a62a2f871bc00614ba160ae98829f
mxch18 <maxens.achiepi@gmx.com>
2023-03-04 19:31:06 +0100

    [REFACTOR] - Make all regex pattern verbose

--- tests/text_processor.py
+++ tests/text_processor.py
@@ -6,9 +6,8 @@ if __name__ == '__main__':
                 'kick attempt', 'rush attempt', 'recovered', 'no play',
                 'touchback')
 
-    # extract all patterns from the sentence
     pattern_kickoff = re.compile(r"""
-     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Kicker name
      \skickoff\s
      (?P<nb_yards>\d+)                             # Number of yards kicked
      \syard(s)*\sto\sthe\s
@@ -23,62 +22,118 @@ if __name__ == '__main__':
      (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
      """, re.VERBOSE)
 
-    pattern_tackle = re.compile(
-     r"(?<=(\(|\;))[[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+(?=(\)|\;))")
-    # player pattern preceded by ( or ; and followed by ) or ;
+    pattern_tackle = re.compile(r"""
+     (?<=(\(|\;))                                  # ( or ; preceding
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     (?=(\)|\;))")                                 # ; or ) following
+    """, re.VERBOSE)
 
-    pattern_extraPoint = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ (kick|rush) attempt (good|failed)")
+    pattern_extraPoint = re.compile(r"""
+    (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+    \s
+    (kick|rush)                                   # Type of extra point
+    \sattempt\s
+    (good|failed)                                 # Good or bad attempt
+    """, re.VERBOSE)
 
     # rush
-    # r"[A-Z]\.[A-Za-z\-]+ rush .*for (((loss of )*\d+ yard(s)*)|(no gain)) to the [A-Z]+\d+"
-    pattern_rush_gain = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ rush.*for \d+ yard(s)* to the [A-Z]+\d+")
-    pattern_rush_noGain = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ rush.*for no gain to the [A-Z]+\d+")
-    pattern_rush_loss = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ rush.*for loss of \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_rush_gain = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Rusher name
+     \srush.*for\s
+     (?P<nb_yards>\d+)                             # Number of Yards gained
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+     """, re.VERBOSE)
+    pattern_rush_noGain = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Rusher name
+     \srush.*for\sno\sgain\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+     """, re.VERBOSE)
+    pattern_rush_loss = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Rusher name
+     \srush.*for\sloss\sof\s
+     (?P<nb_yards>\d+)                             # Number of Yards lost
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+     """, re.VERBOSE)
 
     pattern_firstDown = re.compile(
      r"1ST DOWN")
 
-    pattern_pass_complete = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+.*pass complete to [A-Z]\.[A-Za-z\-]+ for \d+ yard(s)* to the [A-Z]+\d+")
-    pattern_pass_incomplete = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+.*pass incomplete( to [A-Z]\.[A-Za-z\-]+)*")
+    pattern_pass_complete = re.compile(r"""
+     (?P<player_passing>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)   # Passer name
+     .*pass\scomplete\sto\s
+     (?P<player_receiving>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+) # Receiver name
+     \sfor\s
+     (?P<nb_yards>\d+)                                    # Number of Yards
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                                  # Team Name Yard Line
+    """, re.VERBOSE)
+    pattern_pass_incomplete = re.compile(r"""
+     (?P<player_passing>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)          # Passer name
+     .*pass\sincomplete\s
+     (to\s(?P<player_receiving>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+))* # Receiver name
+    """, re.VERBOSE)
 
-    pattern_run = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ for \d+ yard(s)*"
-    )
+    pattern_run = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     \sfor\s
+     (?P<nb_yards>\d+)                             # Number of Yards
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+    """, re.VERBOSE)
 
-    pattern_sack = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ sacked for loss of \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_sack = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     \ssacked\sfor\sloss\sof\s
+     (?P<nb_yards>\d+)                             # Number of Yards
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+    """, re.VERBOSE)
 
-    pattern_punt = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ punt \d+ yard(s)* to the [A-Z]+\d+")
+    pattern_punt = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     \spunt\s
+     (?P<nb_yards>\d+)                             # Number of Yards
+     \syard(s)*\sto\sthe\s
+     (?P<tnyl>[A-Z]+\d+)                           # Team Name Yard Line
+    """, re.VERBOSE)
 
-    pattern_fg = re.compile(
-     r"[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+ field goal attempt from \d+ (GOOD|MISSED)")
+    pattern_fg = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     \sfield\sgoal\sattempt\sfrom\s
+     (?P<nb_yards>\d+)                             # Number of Yards
+     \s
+     (?P<result>GOOD|MISSED)                       # Good or missed
+    """, re.VERBOSE)
 
-    pattern_fairCatch = re.compile(
-     r"")
+    pattern_fairCatch = re.compile(r"""
+     fair\scatch\sby\s
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+    """, re.VERBOSE)
+    pattern_touchback = re.compile(r"""
+     touchback
+    """, re.VERBOSE)
 
-    pattern_safety = re.compile(
-     r"")
+    pattern_safety = re.compile(r"""
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)    # Player name
+     \ssafety
+    """, re.VERBOSE)
 
     pattern_fumble_forced = re.compile(r"""
      fumble\sforced\sby\s
-     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)
+     (?P<player>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)            # Player forcing
     """, re.VERBOSE)
     pattern_fumble = re.compile(r"""
      fumble\sby\s
      (?P<player_fumbling>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+)   # Player fumbling
      \srecovered\sby\s
-     (?P<team_recovering>[A-Z]+)                            # Team recovering
+     (?P<team_recovering>[A-Z]+)                           # Team recovering
      \s
      (?P<player_recovering>[A-Z]\.([a-z]\.)?[A-Z][a-z\-]+) # Player recovering
      \sat\s
-     (?P<tnyl>[A-Z]+\d+)                                   # Team Name Yard Line
+     # Team Name Yard Line
+     (?P<tnyl>[A-Z]+\d+)
     """, re.VERBOSE)
 
     with open('./test_sentences.txt', 'r') as sentences_file:
