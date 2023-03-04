ff8427d8151af4f4301730ee29fa2176c9a10276
mxch18 <maxens.achiepi@gmx.com>
2023-03-04 19:31:24 +0100

    [FEAT] - Add Player class

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player():
+    def __init__(self, first_name, last_name, nationality="", position=[], age=-1, height=-1, weight=-1):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nationality = nationality
+        self.position = position
+        self.age = age
+        self.height = height
+        self.weight = weight
