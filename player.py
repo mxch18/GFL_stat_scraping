a45f745e5429078e7dff6fc21e39e5f0a388fa0e
mxch18 <maxens.achiepi@gmx.com>
2023-03-13 22:56:28 +0100

    [REFACTOR] - Change age to year of birth in Player class

--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 class Player():
-    def __init__(self, first_name, last_name, nationality="", position=[], age=-1, height=-1, weight=-1):
+    def __init__(self, first_name, last_name, nationality="", position=[], yob=-1, height=-1, weight=-1):
         self.first_name = first_name
         self.last_name = last_name
         self.nationality = nationality
         self.position = position
-        self.age = age
+        self.yob = yob  # year of birth
         self.height = height
         self.weight = weight
