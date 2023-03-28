c5a8bde71793b8166fd73c854675044e38d493e9
mxch18 <maxens.achiepi@gmx.com>
2023-03-28 17:02:15 +0200

    [FEAT] - Add main scraper paramaterized script

---
+++ game_scraping.py
@@ -0,0 +1,34 @@
+from myWebDriver import MyWebDriver
+from homePage import HomePage
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-l", "--league", help="select between gfl and gfl2", choices=['gfl', 'gfl2'])
+    parser.add_argument("-y", "--year", help="select a year", type=int)
+    parser.add_argument(
+        "-g", "--game", help="select a game, format is the same as game URL")
+    args = parser.parse_args()
+
+    browser = MyWebDriver()
+
+    try:
+        hp = HomePage("https://stats.gfl.info/", browser)
+        hp.go_to()
+
+        seasons = hp.get_seasons(args.league, args.year)
+
+        for season in seasons:
+            season.go_to()
+            if args.game:
+                games = [season.get_specific_game(args.game)]
+            else:
+                games = season.get_games()
+
+            for game in games:
+                if game:
+                    game.go_to()
+                    game.get_game()
+    finally:
+        browser.quit()
