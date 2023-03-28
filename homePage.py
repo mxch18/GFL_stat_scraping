9dc52d332a810670b318f1aca60daa2ed0619524
mxch18 <maxens.achiepi@gmx.com>
2023-03-28 17:00:07 +0200

    [FEAT] - Allow selection of league and/or year in get_seasons

--- homePage.py
+++ homePage.py
@@ -1,23 +1,40 @@
 from selenium.webdriver.common.by import By
 from myPage import MyPage
 from seasonPage import SeasonPage
+import re
 
 
 class HomePage(MyPage):
     def __init__(self, url, webdriver):
         super().__init__(url, webdriver)
 
-    def get_seasons(self):
+    def get_seasons(self, league=None, year=None):
         season_pages = []  # list of SeasonPage object
         season_links = self.driver.find_elements(
             By.XPATH, '//table/tbody/tr/td/a[@href]')
         for link in season_links:
-            season_pages.append(
-                SeasonPage(
-                    link.get_attribute('href'),
-                    self.driver
-                )
-            )
+            league_year = re.findall(
+                r"gfl[0-9]*/[0-9]+", link.get_attribute('href'))[0]
+            if league_year:
+                ly_split = league_year.split("/")
+                link_league = ly_split[0]
+                link_year = int(ly_split[1])
+
+                nok_league = league and link_league != league
+                nok_year = year and link_year != year
+
+                if nok_league or nok_year:
+                    pass
+                else:
+                    season_pages.append(
+                        SeasonPage(
+                            link.get_attribute('href'),
+                            self.driver,
+                            link_league,
+                            link_year
+                            )
+                    )
+
         return season_pages
 
 
